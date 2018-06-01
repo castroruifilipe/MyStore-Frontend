78140757481fa87527a8c515f7bdfdfee32076ab
Rui Leite <a75551@alunos.uminho.pt>
2018-06-01 09:56:48 +0100

    Home e Routes

--- mystore/src/App.js
+++ mystore/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
 
+import * as routes from './constants/routes';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<NavBar />
+				<BrowserRouter >
+					<div>
+						<NavBar />
+						<Switch>
+							<Route exact path={routes.HOME} />
+						</Switch>
+					</div>
+				</BrowserRouter >
 			</div>
 		);
 	}
---
+++ mystore/src/scenes/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+class Home extends Component {
+    
+    render() {
+        return ();
+    }
+}
+
+export default(Home);

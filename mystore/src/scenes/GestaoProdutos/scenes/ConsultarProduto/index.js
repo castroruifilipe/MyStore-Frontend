01edbac521662a09c084a2ec1c9eddbff8da652a
DiogoMachadoA75399 <a75399@alunos.uminho.pt>
2018-06-27 18:45:19 +0100

    Gestão produto - Editar produto

--- mystore/src/scenes/GestaoProdutos/scenes/ConsultarProduto/index.js
+++ mystore/src/scenes/GestaoProdutos/scenes/ConsultarProduto/index.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import { Row, Col, Button } from 'reactstrap';
+import PencilIcon from 'react-icons/lib/fa/edit';
+
 import * as services from '../../../../services/produtos';
+import { formatterPrice } from '../../../../constants/formatters';
+import EditarProduto from '../EditarProduto';
 
 class ConsultarProduto extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            produto: undefined,
+            codigo: undefined,
+            nome: undefined,
+            categoria: undefined,
+            stock: undefined,
+            descricao: undefined,
+            precoBase: undefined,
+            valorDesconto: undefined,
+            precoFinal: undefined,
+            estatisticasVendas: {},
+            modoEdicao: false,
         }
     }
 
     componentWillMount() {
         services.getProduto(this.props.match.params.codigo)
             .then(response => {
-                this.setState({ produto: response.data })
+                let data = response.data;
+                this.setState({
+                    codigo: data.codigo,
+                    nome: data.nome,
+                    categoria: data.categoria,
+                    stock: data.stock,
+                    descricao: data.descricao,
+                    precoBase: data.precoBase,
+                    desconto: data.valorDesconto,
+                    precoFinal: data.precoFinal,
+                    estatisticasVendas: {
+                        ...data.estatisticasVendas
+                    },
+                })
             })
             .catch(error => {
                 if (error.response) {
@@ -26,9 +52,32 @@ class ConsultarProduto extends Component {
             });
     }
 
+    guardar = () => {
+        alert("aquii");
+    }
+
+    editar = () => {
+        this.setState({ modoEdicao: true })
+    }
+
+    cancelar = () => {
+        this.setState({ modoEdicao: false, ...this.INITIAL_STATE })
+    }
+
+
     render() {
-        let produto = this.state.produto;
-        if (!produto) {
+        let {
+            codigo,
+            nome,
+            categoria,
+            stock,
+            descricao,
+            precoBase,
+            desconto,
+            precoFinal,
+            estatisticasVendas
+        } = this.state;
+        if (codigo === undefined) {
             return null;
         }
         return (
@@ -36,18 +85,83 @@ class ConsultarProduto extends Component {
                 <Col>
                     <Row>
                         <Col>
-                            <h3>Detalhes do Produto</h3>
+                            <h3 className='headerColor'>Detalhes do Produto</h3>
+                            <h6>Produto #{codigo}</h6>
+                        </Col>
+                        <Col align="center">
+                            {
+                                this.state.modoEdicao ?
+                                    <div>
+                                        <Button color="success" className="mr-2 block inline-md" style={{ width: '180px' }} onClick={this.guardar}>
+                                            <PencilIcon className="mr-1" />
+                                            Guardar
+                                        </Button>
+                                        <Button color="secondary" className="mr-2 block inline-md" style={{ width: '180px' }} onClick={this.cancelar}>
+                                            Cancelar
+                                        </Button>
+                                    </div>
+                                    :
+                                    <Button color="primary" className="mr-2 block inline-md" style={{ width: '180px' }} onClick={this.editar}>
+                                        <PencilIcon className="mr-1" />
+                                        Editar Produto
+                                    </Button>
+                            }
                         </Col>
                     </Row>
-                    <Row className="mt-3">
-                        <Col>
-                            <h4>{produto.nome}</h4>
-                            <h6>Categoria:{produto.categoria.descricao}</h6>
-                        </Col>
-                        <Col>
-                            <Button color="primary">Editar Produto</Button>
-                        </Col>
-                    </Row>
+                    {
+                        this.state.modoEdicao ?
+                            <EditarProduto produto={this.state} onChange={this.onChange} />
+                            :
+                            <div>
+                                <Row className="mt-3">
+                                    <Col>
+                                        <h5>{nome}</h5>
+                                        <h6><strong>Categoria:</strong> {categoria.descricao}</h6>
+                                    </Col>
+                                    <Col align="center">
+                                        <h6><strong>Stock:</strong> {stock} unidades</h6>
+                                    </Col>
+                                </Row>
+                                <Row className="mt-2">
+                                    <Col md="3" align="center">
+                                        <div>
+                                            <img src="https://i.imgur.com/t7DTziH.jpg" alt="Imagem do produto" className="img-fluid" />
+                                        </div>
+                                    </Col>
+                                    <Col md="7">
+                                        <div className="d-inline-block text-justify pt-3">
+                                            {descricao}
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className="mt-4">
+                                    <Col>
+                                        <span><strong>Preço Base:</strong> {formatterPrice.format(precoBase)}</span> <br />
+                                    </Col>
+                                    <Col>
+                                        <span><strong>Desconto:</strong> {desconto === 0 ? 'Sem promoções associadas' : formatterPrice.format(desconto)}</span> <br />
+                                    </Col>
+                                    <Col>
+                                        <span><strong>Preço Final:</strong> {formatterPrice.format(precoFinal)}</span>
+                                    </Col>
+                                </Row>
+
+                                <Row className="mt-4">
+                                    <Col>
+                                        <h4 className='headerColor'>Análise de Vendas</h4>
+                                    </Col>
+                                </Row>
+                                <Row className="mt-2">
+                                    <Col>
+                                        <span><strong>Número de unidades encomendadas:</strong> {estatisticasVendas.numeroEncomendas}</span> <br />
+                                        <span><strong>Número de unidades vendidas em promoção:</strong> {estatisticasVendas.numeroVendasPromocao}</span> <br />
+                                    </Col>
+                                    <Col>
+                                        <span><strong>Número de unidades efitivamente vendidas:</strong> {estatisticasVendas.numeroVendas}</span> <br />
+                                        <p><strong>Total faturado:</strong> {formatterPrice.format(estatisticasVendas.totalFaturado)}</p>
+                                    </Col>
+                                </Row>
+                            </div>}
                 </Col>
             </Row>
         );
---
+++ mystore/src/scenes/GestaoProdutos/scenes/EditarProduto/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Row, Col, Input } from 'reactstrap';
+
+import * as services from '../../../../services/categorias';
+
+class EditarProduto extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: this.props.produto.nome,
+            categoria: this.props.produto.categoria,
+            stock: this.props.produto.stock,
+            descricao: this.props.produto.descricao,
+            precoBase: this.props.produto.precoBase,
+            categorias: [],
+        }
+    }
+
+    componentWillMount() {
+        services.getCategorias()
+            .then(response => {
+                let data = [];
+                response.data.forEach(c =>
+                    data.push({ key: c.id, descricao: c.descricao })
+                );
+                this.setState({ categorias: data });
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.response);
+                    this.setState({ error: error.response.data.message });
+                } else {
+                    console.error(error);
+                }
+            });
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
+    render() {
+        let {
+            nome,
+            categoria,
+            stock,
+            descricao,
+            precoBase,
+        } = this.state;
+        return (
+            <div>
+                <Row className="mt-3">
+                    <Col>
+                        <div className="form-label-group">
+                            <Input value={nome} placeholder="Nome" type="text" className="form-control" id="nome" onChange={this.onChange} />
+                            <label htmlFor="nome">Nome</label>
+                        </div>
+                        <div className="form-label-group">
+                            <Input value={precoBase} placeholder="Preço Base" type="text" className="form-control" id="precoBase" onChange={this.onChange} />
+                            <label htmlFor="precoBase">Preço base</label>
+                        </div>
+                        <div className="form-label-group">
+                            <Input value={stock} placeholder="Stock" type="text" className="form-control" id="stock" onChange={this.onChange} />
+                            <label htmlFor="stock">Stock</label>
+                        </div>
+                        <p className="mb-1">Categoria:</p>
+                        <select className="form-control" id="categoria" value={categoria.descricao} onChange={this.onChange}>
+                            {this.state.categorias.map(o => <option key={o.key}>{o.descricao}</option>)}
+                        </select>
+                    </Col>
+                    <Col>
+                        <div className="form-group">
+                            <label htmlFor="descricao">Descrição:</label>
+                            <textarea className="form-control" rows="8" id="descricao" onChange={this.onChange} value={descricao}
+                                placeholder="Descrição"></textarea>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+
+}
+
+export default EditarProduto;

3e161b4a1f95cec2458eb3ac6fa548a0a7a121cf
Rui Leite <a75551@alunos.uminho.pt>
2018-06-27 17:22:31 +0100

    Categorias

--- mystore/src/scenes/GestaoCategorias/index.js
+++ mystore/src/scenes/GestaoCategorias/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Row, Button, Col } from 'reactstrap';
+import { Row, Button, Col, Container, Alert, Modal, ModalBody, ModalHeader, ModalFooter, Form, Input } from 'reactstrap';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { TiArrowSortedDown, TiArrowSortedUp, TiArrowUnsorted } from 'react-icons/lib/ti';
+import { inject, observer } from 'mobx-react';
+import { compose } from 'recompose';
 
 import * as services from '../../services/categorias';
 
@@ -11,6 +13,9 @@ class GestaoCategorias extends Component {
         super(props);
         this.state = {
             categorias: [],
+            showAlert: false,
+            showModal: false,
+            descricao: '',
         }
     }
 
@@ -20,8 +25,30 @@ class GestaoCategorias extends Component {
             .catch(error => console.error(error));
     }
 
-    apagarCategoria = (id) => {
-        console.log(id);
+    apagarCategoria = (descricao) => {
+        services.deleteCategoria(descricao, this.props.sessionStore.accessToken)
+            .then(response => services.getCategorias())
+            .then(response => this.setState({ categorias: response.data }))
+            .catch(error => {
+                console.error(error.response);
+                this.setState({ showAlert: true })
+            });
+    }
+
+    criarCategoria = () => {
+        services.criarCategoria(this.state.descricao, this.props.sessionStore.accessToken)
+            .then(response => this.toggleModal())
+            .then(() => services.getCategorias())
+            .then(response => this.setState({ categorias: response.data }))
+            .catch(error => console.error(error.response));
+    }
+
+    toggleAlert = () => {
+        this.setState({ showAlert: !this.state.showAlert });
+    }
+
+    toggleModal = () => {
+        this.setState({ showModal: !this.state.showModal });
     }
 
     getCaret = (direction) => {
@@ -39,32 +66,58 @@ class GestaoCategorias extends Component {
     }
 
     buttonFormatter = (cell, row) => {
-        return <Button size="sm" color="danger" onClick={(e) => this.apagarCategoria(row, e)}>Apagar</Button>
+        return <Button size="sm" color="danger" onClick={(e) => this.apagarCategoria(row.descricao, e)}>Apagar</Button>
     }
 
     render() {
         return (
-            <Row>
-                <Col >
-                    <Row>
-                        <Col className="p-0">
-                            <h4>Gestão de Categorias</h4>
-                        </Col>
-                    </Row>
-                    <Row className="mt-3">
-                        <Col className="p-0">
-                            <BootstrapTable version='4' data={this.state.categorias} pagination >
-                                <TableHeaderColumn isKey dataField='id' dataSort caretRender={this.getCaret} width="15%" filter={{ type: 'TextFilter' }} className='customHeader' dataAlign="center">Código</TableHeaderColumn>
-                                <TableHeaderColumn dataField='descricao' dataSort caretRender={this.getCaret} className="customHeader" dataAlign="center">Descrição</TableHeaderColumn>
-                                <TableHeaderColumn dataField='button' dataAlign="center" dataFormat={this.buttonFormatter} width='15%' className="customHeader"></TableHeaderColumn>
-                            </BootstrapTable>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
+            <Container>
+                <Alert color="danger" isOpen={this.state.showAlert} toggle={this.toggleAlert} style={{ position: 'fixed', top: '40px', zIndex: '1' }}>
+                    Não foi possível apagar concluir a operação. Verifique se existem produtos com a categoria que tentou apagar.
+                </Alert>
+                <Row>
+                    <Col md="6" className="p-0">
+                        <h4>Gestão de Categorias</h4>
+                    </Col>
+                    <Col md="6" className="p-0" align="right">
+                        <Button color="primary" onClick={this.toggleModal}>Nova categoria</Button>
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Col className="p-0">
+                        <BootstrapTable version='4' data={this.state.categorias} pagination >
+                            <TableHeaderColumn isKey dataField='id' dataSort caretRender={this.getCaret} width="15%" filter={{ type: 'TextFilter' }} className='customHeader' dataAlign="center">Código</TableHeaderColumn>
+                            <TableHeaderColumn dataField='descricao' dataSort caretRender={this.getCaret} className="customHeader" dataAlign="center">Descrição</TableHeaderColumn>
+                            <TableHeaderColumn dataField='button' dataAlign="center" dataFormat={this.buttonFormatter} width='15%' className="customHeader"></TableHeaderColumn>
+                        </BootstrapTable>
+                    </Col>
+                </Row>
+
+                <Modal isOpen={this.state.showModal} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Criar nova categoria</ModalHeader>
+                    <ModalBody align="center">
+                        <Form className="form-sign">
+                            <div className="form-label-group">
+                                <Input required value={this.state.descricao} placeholder="Descrição da categoria" type="text"
+                                    className="form-control" id="descricao"
+                                    onChange={e => this.setState({ 'descricao': e.target.value })} />
+                                <label htmlFor="descricao" align="left">Descrição da categoria</label>
+                            </div>
+                        </Form >
+
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.criarCategoria}>Criar</Button>{' '}
+                        <Button color="secondary" onClick={this.toggleModal}>Cancelar</Button>
+                    </ModalFooter>
+                </Modal>
+            </Container>
         );
 
     }
 }
 
-export default GestaoCategorias;
+export default compose(
+    inject('sessionStore'),
+    observer
+)(GestaoCategorias);
--- mystore/src/services/categorias/index.js
+++ mystore/src/services/categorias/index.js
@@ -4,3 +4,13 @@ import { urlCategorias } from '../../services';
 
 export const getCategorias = () =>
     axios.get(`${urlCategorias}`);
+
+export const criarCategoria = (descricao, token) =>
+    axios.post(`${urlCategorias}/criar`, { descricao },
+        { headers: { 'Authorization': 'Bearer ' + token } })
+
+export const deleteCategoria = (descricao, token) =>
+    axios.delete(`${urlCategorias}/apagar`, {
+        params: { descricao },
+        headers: { 'Authorization': 'Bearer ' + token }
+    });
